1bed7706e757aab7fa2f4d9d1fd74a9588a2bea8
Pro-gram <stevenjeffers@live.ca>
2023-11-29 22:55:54 -0500

    initializing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+// vite.config.js
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-});
+export default {
+  plugins: [react()],
+  // Add the following favicon option
+  // Change the path to match the location of your custom favicon.svg
+  // Set to false to disable favicon handling completely
+  // https://vitejs.dev/config/#build-assetsinclude
+  build: {
+    assetsInclude: "favicon.svg",
+  },
+};
